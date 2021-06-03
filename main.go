54bc09822c5b64c0401c947e1afee86724b0442c
khisby al ghofari <khisby@gmail.com>
2021-06-04 01:25:30 +0700

    add register endpoint

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"khisoft_campign/handler"
 	"khisoft_campign/user"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -11,36 +11,21 @@ import (
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/khisoft_campaign?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is good")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/khisoft_campaign?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	var users []user.User
-	db.Find(&users)
 
-	c.JSON(http.StatusOK, users)
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
 }
