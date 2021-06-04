5dca3573b09d5104050ff57075737c5948f91ffb
khisby al ghofari <khisby@gmail.com>
2021-06-04 07:05:58 +0700

    add login endpoint

--- handler/user.go
+++ handler/user.go
@@ -40,3 +40,29 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	LoggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(LoggedinUser, "tokennyaaa")
+	response := helper.APIResponse("Succesfully Loggined", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+
+}
